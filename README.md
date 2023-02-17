7d6d01e8acb60680a697d8264888cff1bb53ec73
martynvalenz <martynvalenz@gmail.com>
2023-02-16 21:48:33 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Productive integration
+### Install application
+```
+npm run install
+```
+### Copy .env.example to .env
+
+### Run application
+```
+npm run start
+```
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+const app = express();
+// connect();
+
+app.get('/', (req, res) => {
+  res.json({message:'Hello World!'})
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port} ⚡️`);
+});
